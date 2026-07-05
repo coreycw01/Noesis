26fa41d831b36c94ec311fde573050936a9d8f09
Corey Codex <coreycw01@users.noreply.github.com>
2026-07-05 14:05:42 -0400

    push

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
+    '10.0.0.232',
+    '10.0.0.232:9002',
+    '127.0.0.1',
+    '127.0.0.1:9002',
+    'localhost',
+    'localhost:9002',
     '*.cloudworkstations.dev',
     '9000-firebase-studio-1782092587116.cluster-rbhjeem4mfgjwrkwwvustjr6em.cloudworkstations.dev',
     '6000-firebase-studio-1782092587116.cluster-rbhjeem4mfgjwrkwwvustjr6em.cloudworkstations.dev',
